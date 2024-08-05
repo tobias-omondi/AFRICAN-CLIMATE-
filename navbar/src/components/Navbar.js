72d7a9f05830e49c94142bb5762d25d229b04e89
tobias <tobias.omondi@student.moringaschool.com>
2024-08-05 14:12:22 +0300

    Navbar

--- navbar/src/components/Navbar.js
+++ navbar/src/components/Navbar.js
@@ -1,13 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { AppBar, Toolbar, IconButton, Typography, Stack, Button, Menu, MenuItem } from '@mui/material';
 import { Link } from 'react-router-dom';
 import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
 import MenuIcon from '@mui/icons-material/Menu';
-import './Navbar.css'
+import './Navbar.css';
 
 const Navbar = () => {
-
-      // State for menu anchor element
   const [anchorEl, setAnchorEl] = useState(null);
 
   const openMenu = (event) => {
@@ -20,111 +18,104 @@ const Navbar = () => {
 
   return (
     <div>
-      <AppBar position='fixed'  sx={{ backgroundColor: 'white' }}>
-        <Toolbar> 
-            {/* logo and the title */}
-            <IconButton
+      <AppBar position='fixed' sx={{ backgroundColor: 'white' }}>
+        <Toolbar sx={{justifyContent:'space-between'}}>
+          {/* Logo and title for mobile phone */}
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="logo"
+            sx={{ color: 'blue', display: { xs: 'flex', md: 'none' } }}
+          >
+            <CatchingPokemonIcon />
+          </IconButton>
+          
+          {/* Menu for mobile phones */}
+          <Stack direction='row' spacing={3} sx={{ color: 'black', display: { xs: 'flex', md: 'none' } }}>
+            <IconButton size="large" edge="start" color="inherit" sx={{ color: 'black' }} onClick={openMenu}>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={closeMenu}
+              PaperProps={{
+                sx: {
+                  width: '250px',
+                },
+              }}
+              sx={{ color: 'blue', display: { xs: 'flex', md: 'none' } }}
+            >
+              <MenuItem onClick={closeMenu}>
+                <Link to="/">HOME</Link>
+              </MenuItem>
+              <MenuItem onClick={closeMenu}>
+                <Link to="/about">ABOUT</Link>
+              </MenuItem>
+              <MenuItem onClick={closeMenu}>
+                <Link to="/news">NEWS</Link>
+              </MenuItem>
+              <MenuItem onClick={closeMenu}>
+                <Link to="/documentaries">DOCUMENTARIES</Link>
+              </MenuItem>
+              <MenuItem onClick={closeMenu}>
+                <Link to="/podcast">PODCAST</Link>
+              </MenuItem>
+              <MenuItem onClick={closeMenu}>
+                <Link to="/contact">CONTACT</Link>
+              </MenuItem>
+            </Menu>
+          </Stack>
+
+          {/* Logo and title for desktop */}
+          <IconButton
             size="large"
             edge='start'
             color='inherit'
             aria-label='logo'
-            sx={{ color: 'blue', display: { xs: 'none', md: 'flex' } }}>
-            </IconButton>
+            sx={{ color: 'blue', display: { xs: 'none', md: 'flex' } }}
+          >
             <CatchingPokemonIcon />
-            <Typography
+          </IconButton>
+          <Typography
             variant="h5"
             component="div"
             sx={{ flexGrow: 1, color: 'black', display: { xs: 'none', md: 'flex' } }}
-            >
-                My Web
-            </Typography>
-                {/* navigation buttons & Links for DEsktop */}
-                <Stack direction="row" spacing={5} sx={{ color: 'black', display: { xs: 'none', md: 'flex' }, textDecoration:'none', mr:'15rem' }}
-                className='Navbar_desctop_menu'>
-                    <Button color='inherit'>
-                        <Link to='/'>HOME</Link>
-                    </Button>
+          >
+            My Web
+          </Typography>
 
-                    <Button color='inherit'>
-                        <Link to='about'>About</Link>
-                    </Button>
-
-                    <Button color='inherit'>
-                        <Link to='news'>NEWS</Link>
-                    </Button>
-
-                    <Button color='inherit'>
-                        <Link to='documentaries'>DOCUMENTARIES</Link>
-                    </Button>
-
-                    <Button color='inherit'>
-                        <Link to='podcast'>PODCAST</Link>
-                    </Button>
-
-                    <Button color='inherit'>
-                        <Link to='contact'>CONTACT</Link>
-                    </Button>
-                </Stack>
-
-                {/* menu for mobile phones  */}
-                <Stack direction='row' spacing={3} sx={{color: 'black', display: { xs: 'flex', md: 'none' }}}>
-                <IconButton size="large" edge="start" color="inherit" sx={{ color: 'black' }} onClick={openMenu}>
-                   <MenuIcon />
-                </IconButton>
-                <Menu
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={closeMenu}
-                PaperProps={{
-                    sx: {
-                      width: '250px', // Set your desired width here
-                    }
-                }}
-                     sx={{ color: 'blue', display: { xs: 'flex', md: 'none'} }}
-                     >
-                    <MenuItem onClick={closeMenu}>
-                       <Link to="/">HOME</Link>
-                      </MenuItem>
-
-                    <MenuItem onClick={closeMenu}>
-                      <Link to="about" >ABOUT</Link>
-                    </MenuItem>
-
-                    <MenuItem onClick={closeMenu}>
-                      <Link to="news">NEWS</Link>
-                     </MenuItem>
-
-                    <MenuItem onClick={closeMenu}>
-                      <Link to="documentaries">DOCUMENTARIES</Link>
-                    </MenuItem>
-
-                    <MenuItem onClick={closeMenu}>
-                      <Link to="podcats">PODCAST</Link>
-                    </MenuItem>
-
-                    <MenuItem onClick={closeMenu}>
-                      <Link to="contacts">CONTACT</Link>
-                    </MenuItem>
-                </Menu>
-                </Stack>
-
-                {/* logo title for mobile phone */}
-                <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="logo"
-                sx={{ color: 'blue', display: { xs: 'flex', md: 'none' }, ml:'12rem' }}
-                 >
-                <CatchingPokemonIcon />
-                </IconButton>
-          {/* <Typography variant="h5" component="div" sx={{ flexGrow: 1, color: 'black', display: { xs: 'flex', md: 'none' } }}>
-          Web
-        </Typography> */}
+          {/* Navigation buttons & Links for Desktop */}
+          <Stack
+            direction="row"
+            spacing={5}
+            sx={{ color: 'black', display: { xs: 'none', md: 'flex' }, textDecoration: 'none', mr: '15rem' }}
+            className='Navbar_desctop_menu'
+          >
+            <Button color='inherit'>
+              <Link to='/'>HOME</Link>
+            </Button>
+            <Button color='inherit'>
+              <Link to='about'>About</Link>
+            </Button>
+            <Button color='inherit'>
+              <Link to='news'>NEWS</Link>
+            </Button>
+            <Button color='inherit'>
+              <Link to='documentaries'>DOCUMENTARIES</Link>
+            </Button>
+            <Button color='inherit'>
+              <Link to='podcast'>PODCAST</Link>
+            </Button>
+            <Button color='inherit'>
+              <Link to='contact'>CONTACT</Link>
+            </Button>
+          </Stack>
         </Toolbar>
       </AppBar>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
