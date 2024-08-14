d9ac0070c464e6163bee81039f0d8b59081fca0e
tobias <tobias.omondi@student.moringaschool.com>
2024-08-14 13:58:27 +0300

    changed files

--- navbar/src/components/Home.js
+++ navbar/src/components/Home.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './Home.css';
-import homeimg from '../components/ASSET/wildlife.jpeg';
 import section1 from '../components/ASSET/Rich nations pledge funds at climate crisis summit.jpeg';
 import section2 from '../components/ASSET/Download Detailed Map of Africa Continent in Black Silhouette for free.jpeg';
+import { Button } from 'react-scroll';
 
 const Home = () => {
   return (
-    <div className='home_container' style={{ paddingTop: '64px' }}> {/* Adjust padding based on Navbar height */}
-      <img src={homeimg} className='image-home' alt="Wildlife" />
+    <>
+      <div className='home_container' style={{ paddingTop: '64px' }}>
+        <div className='landing-container'>
+        <h2>DELIVERING CLIMATIC<br/> TO AFRICAN CONTINENT.</h2>
+        <Button type='submit'>Sign-Up</Button>
+      </div>
+      </div>
       
       <div className='section-img-container'>
         <img src={section1} alt="Section 1" className='section1-img' />
-        <p>Our mission is to empower communities and stakeholders by providing accessible resources, expert insights, and a collaborative space for dialogue. We aim to amplify African voices in the global climate conversation and inspire actionable solutions for a sustainable future. Feel free to adjust any wording to better fit your website's tone!</p>
-        </div>
-        <div>
-        <img src={section2} alt="Section 2" className='section2-img'  />
+        <p>Our mission is to empower communities and stakeholders by providing accessible resources, expert insights, and a collaborative space for dialogue...</p>
       </div>
-    </div>
+      <img src={section2} alt="Section 2" className='section2-img' />
+    </>
   );
 }
 
