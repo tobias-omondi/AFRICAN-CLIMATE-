7730181eeedefa236ae4383f6aefc970cd47d9f3
tobias <tobias.omondi@student.moringaschool.com>
2024-08-20 13:11:31 +0300

    News page

--- navbar/src/components/News.js
+++ navbar/src/components/News.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import './News.css'
+import Footer from './Footer'
 
 const News = () => {
   return (
-    <div>
-      <h1>News</h1>
-      <p>This is news page</p>
+    <>
+    <div className='news_page'>
+      <div className='news_heading'>
+      <h1>NEWS</h1>
+      <p>"Vintage Climatic News brings you the latest insights, stories, and developments 
+         at the intersection of climate awareness and vintage culture. We highlight the importance 
+         of sustainability through the lens of history, showcasing how past practices can inspire a greener,
+         more mindful future."</p>
+      </div>
     </div>
+    <Footer />
+    </>
   )
 }
 
